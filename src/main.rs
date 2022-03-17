4c2e54f4ca32185d87e7bb26ee567459b310e266
itisthecon <itisthecon@gmail.com>
2022-03-17 19:54:54 +0800

    read dir recursive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    read_dir("./");
+}
+
+fn read_dir(dir: &str) {
+    let paths = fs::read_dir(dir).unwrap();
+
+    for entry in paths {
+        let path = entry.unwrap().path();
+        let metadata = fs::metadata(&path).unwrap();
+        if metadata.is_file() {
+            println!("File: {} Length: {}", path.display(), metadata.len());
+        } else if metadata.is_dir() {
+            println!("Dir: {}", path.display());
+            read_dir(path.to_str().unwrap());
+        }
+    }
 }
